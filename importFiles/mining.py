a7f1e3903973e5ffcbc6707251f6f821970b66af
Victor Ramirez <60759917+VictorRamrez@users.noreply.github.com>
2021-08-10 08:08:40 -0700

    Create mining.py

---
+++ importFiles/mining.py
@@ -0,0 +1,26 @@
+import hashlib
+
+def mining_block(block_list, root_hash):
+    #Creates a for loop were in range of 0-11
+    #Will add number to block_list and create a hash block
+    for number in range(10000):
+        # Creates a new list with block_list in it
+        list_of_transactions = [block_list]
+        # start of with number 1
+        nonce = str(number)
+        #Append number to block_list
+        list_of_transactions.append(nonce)
+        # join list together
+        new_block_list = "".join(list_of_transactions)
+        # Encodes block list
+        encoded = new_block_list.encode()
+        #Creates hash block
+        nonce_hash = hashlib.sha256(encoded).hexdigest()
+        #Create an if statement:
+        #if hash does not start off with 5 zeros    
+        if nonce_hash.startswith("000") == True and nonce_hash < root_hash:
+            print("WOW you Actually did it!!!!")
+            break
+        #else:
+            #print(number)
+    return nonce, new_block_list, nonce_hash
